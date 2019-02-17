65b7b88c4e5012f5318907c5ef8e1bae37f28dc2
MariiaMoroz <47716153+MariiaMoroz@users.noreply.github.com>
2019-02-17 17:10:41 +0200

    Add files via upload

---
+++ DividerList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DividerList
+{
+	class DividerList
+	{
+		public static void Main()
+		{
+			bool flag = true;
+			while (flag)
+			{
+				Console.WriteLine("Write number:");
+				if (int.TryParse(Console.ReadLine(), out int number))
+				{
+					Console.WriteLine("Divider List:");
+					Console.WriteLine(GetDividers(number));
+					flag = false;
+				}
+			}
+			Console.ReadKey();
+		}
+
+		public static string GetDividers(int number)
+		{
+			string result = null;
+			for (int i = 1; i <= number; i++)
+			{
+				if (number % i == 0) result += i + " ";
+			}
+			return result;
+		}
+	}
+}
